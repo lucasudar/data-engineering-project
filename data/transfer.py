cfa2d6485da2f94db0c1c5a9a8f3dde0b81e03fb
Nikita Starkov <lucasudar@gmail.com>
2024-06-21 16:00:05 -0700

    created data folder with source files and python script to transfer all data to remote DB

---
+++ data/transfer.py
@@ -0,0 +1,39 @@
+import pandas as pd
+from sqlalchemy import create_engine, text
+
+# Define your PostgreSQL connection details
+DB_USER = 'your_username'
+DB_PASSWORD = 'your_password'
+DB_HOST = 'your_host'
+DB_PORT = 'your_port'
+DB_NAME = 'your_database'
+
+# Create the database connection string
+connection_string = f'postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
+
+# Create the SQLAlchemy engine
+engine = create_engine(connection_string)
+
+# Define the schema
+SCHEMA = 'STG'
+
+# Define the paths to your Excel files
+excel_files = ['orders.xls', 'people.xls', 'returns.xls']
+
+
+# Ensure the schema exists
+with engine.connect() as connection:
+    connection.execute(text(f'CREATE SCHEMA IF NOT EXISTS {SCHEMA}'))
+
+# Iterate over each Excel file
+for file in excel_files:
+    # Read the Excel file into a DataFrame
+    df = pd.read_excel(file)
+
+    # Extract the table name from the file name (e.g., file1.xlsx -> file1)
+    table_name = file.split('.')[0]
+
+    # Push the DataFrame to the PostgreSQL database
+    df.to_sql(table_name, engine, schema=SCHEMA, if_exists='replace', index=False)
+    print(f"Data from {file} has been pushed to the {SCHEMA}.{table_name} table.")
+
--- readme.md
+++ readme.md
@@ -24,6 +24,7 @@ Before you start, ensure you have the following installed on your local machine:
 - [dbt](https://www.getdbt.com/)
 - [Snowflake](https://www.snowflake.com/)
 - [Looker](https://looker.com/)
+- [PostgreSQL](https://www.postgresql.org/)
 - Python 3.12
 - Git
 
@@ -33,3 +34,13 @@ Before you start, ensure you have the following installed on your local machine:
    ```bash
    git clone https://github.com/lucasudar/data-engineering-project.git
    cd data-engineering-project
+2. Create DB and schema in Cloud
+3. Setting up the Env
+   ```bash
+   python3 -m venv env
+   source env/bin/activate
+   pip install pandas sqlalchemy psycopg2-binary xlrd openpyxl
+4. Transfer all data to your prepared DB
+   ```bash
+   cd data/
+   python3 trasfer.py
